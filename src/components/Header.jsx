01785938a82941a745a0c8ea6c8101570891cdda
Meagan Butters <meagan.butters@hotmail.com>
2020-11-18 16:30:28 -0500

    Added more projects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -84,11 +84,11 @@ const Header = () => (
                     to="/work">
                     Work
                 </Link>
-                <Link
+                {/* <Link
                     activeClassName="Link--is-active"
                     to="/blog">
                     Blog
-                </Link>
+                </Link> */}
             </HeaderLinks>
         </HeaderContent>
     </HeaderContainer>
